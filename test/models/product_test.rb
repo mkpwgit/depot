5090aed80fbd3cc03529aa5b4ccef8856d187f7b
vgerton <191219k1>
2014-03-19 18:54:44 +0300

    added second controller

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,6 +1,8 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
+
+  fixtures :products
   # test "the truth" do
   #   assert true
   # end
@@ -52,5 +54,17 @@ class ProductTest < ActiveSupport::TestCase
     bad.each do |name|
       assert new_product(name).invalid?, "#{name} shouldn't be valid"
     end
+  end
+
+  test "product is not valid without a unique title" do
+    product = Product.new(title:
+                              products(:ruby).title,
+                          description: "yyy",
+                          price:
+                              1,
+                          image_url:
+                              "fred.gif")
+    assert product.invalid?
+    assert_equal ["has already been taken"], product.errors[:title]
   end
 end
